236fd7b816ae85b8c0397206a711df47d64f8eec
Yves Reynhout <yves.reynhout@gmail.com>
2016-12-13 19:40:50 +0100

    Number of registered players.

--- dotnetcore/Program.cs
+++ dotnetcore/Program.cs
@@ -13,13 +13,14 @@ namespace projections
         {
            var stream_id = 0;
            if(args.Count() > 0) stream_id = int.Parse(args[0]);
-           var raw_data = new RestEventReader().Read($"http://playing-with-projections.herokuapp.com/stream/{stream_id}");
-           // var raw_data = new RestEventReader().Read($"https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/{stream_id}.json");
-           // var raw_data = new FileEventReader().Read($"../data/{stream_id}.json");
+           //var raw_data = new RestEventReader().Read($"http://playing-with-projections.herokuapp.com/stream/{stream_id}");
+           //var raw_data = new RestEventReader().Read($"https://raw.githubusercontent.com/tcoopman/playing_with_projections_server/master/data/{stream_id}.json");
+           var raw_data = new FileEventReader().Read($"../data/{stream_id}.json");
            var events = new JsonEventParser().Parse(raw_data);
            var projector = new Projector(events);
 
            Console.WriteLine("Number of events: {0}", projector.NumberOfEvents);
+           Console.WriteLine("Number of players that registered: {0}", projector.NumberOfRegisteredPlayers);
         }
     }
 
@@ -31,5 +32,13 @@ namespace projections
         this.events = events;
       }
       public int NumberOfEvents{get{ return events.Count();}}
+      public int NumberOfRegisteredPlayers 
+      {
+        get 
+        {
+          return events.Where(@event => @event.type == "PlayerHasRegistered").Count();
+        }
+      }
+      //public string[] DistinctEvents{get{return events.GroupBy()}}
     }
 }
